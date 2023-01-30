f88b3bda1df0b84e8a8646976d0c0ee87b32e03b
TalalMash <0talal.mash0@gmail.com>
2023-01-30 20:35:17 +0300

    Cloned to new repo

---
+++ build_amd64.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+buildarch=amd64
+apt install libext2fs2 debootstrap wget
+fallocate -l 1.8G smoothwan.img
+mkfs.ext4 smoothwan.img
+tune2fs -U 337d0b9f-8716-42b2-b316-312811aef984 smoothwan.img
+mkdir mnt
+mount smoothwan.img mnt/
+cd mnt
+debootstrap --arch=$buildarch buster . http://ftp.de.debian.org/debian
+mount --bind /dev dev
+mount --bind /proc proc
+mount --bind /sys sys
+ln -s /bin/bash bin/ash
+chroot . sh -c "/usr/bin/apt update && apt -y install wget"
+chroot . /usr/bin/wget https://packages.ntop.org/apt-stable/buster/all/apt-ntop-stable.deb
+chroot . /usr/bin/dpkg -i /apt-ntop-stable.deb
+chroot . /usr/bin/apt -y install -f
+chroot . /usr/bin/apt update
+chroot . /usr/bin/apt -y install ntopng
+chroot . /usr/bin/fallocate -l 512M /swapfile
+chroot . /usr/sbin/mkswap /swapfile
+chroot . /usr/bin/chmod 0600 /swapfile
+umount dev
+umount proc
+umount sys
+cd ..
+umount mnt
+gzip smoothwan.img
+mv smoothwan.img.gz smoothwan_debuster_$buildarch.img.gz
---
+++ build_arm64.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+buildarch=arm64
+apt install libext2fs2 debootstrap qemu-user-static wget
+fallocate -l 1.8G smoothwan.img
+mkfs.ext4 smoothwan.img
+tune2fs -U 337d0b9f-8716-42b2-b316-312811aef984 smoothwan.img
+mkdir mnt
+mount smoothwan.img mnt/
+cd mnt
+debootstrap --arch=$buildarch buster . http://ftp.de.debian.org/debian
+mount --bind /dev dev
+mount --bind /proc proc
+mount --bind /sys sys
+ln -s /bin/bash bin/ash
+chroot . sh -c "/usr/bin/apt update && apt -y install wget"
+chroot . /usr/bin/echo "deb https://packages.ntop.org/apt/buster_pi/ arm64/" >> etc/apt/sources.list
+chroot . /usr/bin/echo "deb https://packages.ntop.org/apt/buster_pi/ all/" >> etc/apt/sources.list
+chroot . /usr/bin/apt -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true update
+chroot . /usr/bin/apt-get -y -o APT::Get::AllowUnauthenticated=true install ntopng
+chroot . /usr/bin/fallocate -l 512M /swapfile
+chroot . /usr/sbin/mkswap /swapfile
+chroot . /usr/bin/chmod 0600 /swapfile
+umount dev
+umount proc
+umount sys
+cd ..
+umount mnt
+gzip smoothwan.img
+mv smoothwan.img.gz smoothwan_debuster_$buildarch.img.gz
